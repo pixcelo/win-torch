be952e540386360f62f885445b573dfccfdf5d28
pixcelo <bodomu@gmail.com>
2023-07-21 04:42:58 +0900

    mod: tradingEnvクラス

--- train/trading_env_plotter.py
+++ train/trading_env_plotter.py
@@ -1,49 +1,55 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
 
 class TradingEnvPlotter:
-    def __init__(self, env):
-        self.env = env
+    def __init__(self, action_space):
+        self.action_space = action_space
+        self.total_asset_history = []
+        self.actions_history = []
+        self.episode_rewards = []
+        self.df_history = pd.DataFrame()
+        
+    def update_history(self, history, total_asset_history, actions_history, episode_rewards):
+        self.df_history = pd.DataFrame(history)
+        self.df_history.set_index('step', inplace=True)
+        self.total_asset_history.append(total_asset_history)
+        self.actions_history.append(actions_history)
+        self.episode_rewards.append(episode_rewards)
 
     def plot_history(self):
-        df_history = pd.DataFrame(self.env.history)
-        df_history.set_index('step', inplace=True)
-
         fig, ax = plt.subplots(2, 2, figsize=[8, 6])
 
         # Total Asset over time
-        ax[0][0].plot(self.env.total_asset_history, label='Total Asset', linestyle='-')
+        ax[0][0].plot(self.total_asset_history[-1], label='Total Asset', linestyle='-')
         ax[0][0].yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.0f}"))
         ax[0][0].set_title('Total Asset over Time')
         ax[0][0].legend(loc='upper left')
 
         # Action over time
-        ax[0][1].plot(self.env.actions_history, label='Actions History', linestyle='-')
+        ax[0][1].plot(self.actions_history[-1], label='Actions History', linestyle='-')
         ax[0][1].set_title('Action over time')
         ax[0][1].legend(loc='upper left')
-        ax[0][1].set_yticks(range(self.env.action_space.n))
+        ax[0][1].set_yticks(range(self.action_space.n))
 
         # Cumulative reward plot
-        ax[1][0].plot(np.cumsum(self.env.episode_rewards), label='Cumulative Reward', linestyle='-')
+        ax[1][0].plot(np.cumsum(self.episode_rewards[-1]), label='Cumulative Reward', linestyle='-')
         ax[1][0].set_title('Cumulative Reward per Episode')
         ax[1][0].legend(loc='upper left')
         ax[1][0].autoscale_view(scalex=True, scaley=True)
 
         # Reward over time
-        ax[1][1].plot(self.env.episode_rewards, label='Reward per step', linestyle='-')
-        ax[1][1].set_title('Reward per Step')
+        ax[1][1].plot(self.episode_rewards[-1], label='Reward', linestyle='-')
+        ax[1][1].set_title('Reward over Time')
         ax[1][1].legend(loc='upper left')
-        ax[1][1].autoscale_view(scalex=True, scaley=True)
 
         plt.tight_layout()
         plt.show()
 
-    def plot_stock_price(self):
+    def plot_stock_price(self, df):
         fig, ax = plt.subplots(figsize=[16, 4])
-        ax.plot(self.env.df.index, self.env.df['close'], label='Close Price')
+        ax.plot(df.index, df['close'], label='Close Price')
         ax.set_title('Stock Price')
         ax.legend(loc='upper left')
         plt.tight_layout()
--- train/trading_env_short.py
+++ train/trading_env_short.py
@@ -1,26 +1,11 @@
-import gym
-import numpy as np
-import pandas as pd
 from gym import spaces
+from trading_env import TradingEnv
 
-class TradingEnv(gym.Env):
+class ShortingTradingEnv(TradingEnv):
     def __init__(self, df, initial_cash=100000, transaction_cost=0.001, max_holdings=1):
-        super(TradingEnv, self).__init__()
+        super(ShortingTradingEnv, self).__init__(df, initial_cash, transaction_cost, max_holdings)
 
-        self.df = df
-        self.reward_range = (-np.inf, np.inf)
-        self.action_space = spaces.Discrete(3)  # 0: Do nothing, 1: Buy, 2: Sell
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(self.df.shape[1] + 4,))
-
-        self.initial_cash = initial_cash
-        self.transaction_cost = transaction_cost
-        self.holdings = 0
-        self.max_holdings = max_holdings
-        self.episode_reward = 0
-        self.episode_rewards = []
-        self.actions_history = []
-        self.total_asset_history = []
-        self.reset()
+        self.action_space = spaces.Discrete(5)  # 0: Do nothing, 1: Buy, 2: Sell, 3: Short, 4: Cover
 
     def step(self, action):
         assert self.action_space.contains(action)
@@ -45,6 +30,18 @@ class TradingEnv(gym.Env):
             if self.holdings > 0:
                 self.holdings -= 1
                 self.cash += current_price * (1 - self.transaction_cost)
+            else:
+                reward -= old_total_asset * penalty
+        elif action == 3:  # Short
+            if self.cash >= current_price and self.holdings > -self.max_holdings:  # Adjust here to allow shorting
+                self.holdings -= 1  # Shorting increase the negative holdings
+                self.cash += current_price * (1 - self.transaction_cost)  # And increase the cash
+            else:
+                reward -= old_total_asset * penalty
+        elif action == 4:  # Cover
+            if self.holdings < 0:  # Only can cover if there are shorts
+                self.holdings += 1  # Covering decrease the negative holdings
+                self.cash -= current_price * (1 + self.transaction_cost)  # And decrease the cash
             else:
                 reward -= old_total_asset * penalty
 
@@ -72,15 +69,3 @@ class TradingEnv(gym.Env):
 
         return self._get_observation(), reward, done, {}
 
-    def reset(self):
-        self.cash = self.initial_cash
-        self.holdings = 0
-        self.current_step = 0
-        self.history = []
-        return self._get_observation()
-    
-    def _get_observation(self):
-        total_asset = self.cash + self.holdings * self.df.iloc[self.current_step]['close']
-        cash_ratio = self.cash / total_asset if total_asset > 0 else 0
-        holdings_ratio = self.holdings * self.df.iloc[self.current_step]['close'] / total_asset if total_asset > 0 else 0
-        return np.append(self.df.iloc[self.current_step], [self.holdings, total_asset, cash_ratio, holdings_ratio])
