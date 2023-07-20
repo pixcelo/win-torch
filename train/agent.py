dd9199594d72d5e2147243195643e89c574a5a99
pixcelo <bodomu@gmail.com>
2023-07-21 04:43:42 +0900

    mod: agentクラス

--- train/agent.py
+++ train/agent.py
@@ -1,137 +1,92 @@
 import numpy as np
 import random
 import torch
+import torch.nn as nn
 from torch import optim
 from collections import deque
 from qnetwork import QNetwork
 import datetime
 
 class Agent:
-    def __init__(self, env, state_size, action_size, atom_size=51, gamma=0.99, lr=0.9):
+    def __init__(self, env, gamma=0.99, lr=0.01, batch_size=64, memory_size=10000):
         self.env = env
-        self.state_size = state_size
-        self.action_size = action_size
-        self.atom_size = atom_size
-        self.v_min = -10
-        self.v_max = 10
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.support = torch.linspace(self.v_min, self.v_max, self.atom_size).to(self.device)
-
-        # Initialize Q-Network and target network
-        self.q_network = QNetwork(state_size, action_size, atom_size, self.support, self.v_min, self.v_max).to(self.device)
-        self.target_network = QNetwork(state_size, action_size, atom_size, self.support, self.v_min, self.v_max).to(self.device)
-        self.target_network.load_state_dict(self.q_network.state_dict())
-        self.target_network.eval()
-        # delta_zは、DQNにおけるDistributional DQNの一部となる確率分布の離散化のための間隔
-        self.delta_z = (self.v_max - self.v_min) / (self.atom_size - 1)
-
-        self.optimizer = optim.Adam(self.q_network.parameters(), lr=lr)
         self.gamma = gamma
+        self.batch_size = batch_size
 
-        # epsilon-greedy method
+        self.action_size = self.env.action_space.n
+        self.obs_size = self.env.observation_space.shape[0]
+        self.memory = deque(maxlen=memory_size)
+        self.epsilon = 1.0
         self.epsilon_min = 0.01
-        self.epsilon_max = 1.0
         self.epsilon_decay = 0.995
 
-        # Initialize experience replay memory
-        self.memory = deque(maxlen=2000)
-        self.batch_size = 64
+        self.model = QNetwork(self.obs_size, self.action_size)
+        self.target_model = QNetwork(self.obs_size, self.action_size)
+        self.target_model.load_state_dict(self.model.state_dict())
+        self.target_model.eval()
 
-    # 確率的に行動を選択するε-グリーディー法と、ネットワークによるQ値予測を用いた行動選択を行う
-    def get_action(self, state, episode):
-        # Decrease epsilon over time
-        epsilon = max(self.epsilon_min, self.epsilon_max - self.epsilon_decay * episode)
-        
-        if np.random.rand() <= epsilon:
-            return random.randrange(self.action_size)
-        
-        state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
-        with torch.no_grad():
-            act_values = (self.q_network(state) * self.support).sum(dim=2)
-
-        action = np.argmax(act_values.cpu().data.numpy())
-        return action
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.criterion = nn.MSELoss()
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-    def replay(self):
-        # Sample a minibatch from memory
-        minibatch = random.sample(self.memory, self.batch_size)
-        states, actions, rewards, next_states, dones = zip(*minibatch)        
-        states = torch.from_numpy(np.array(states)).float().to(self.device)
-        actions = torch.tensor(actions).long().unsqueeze(1).to(self.device)
-        rewards = torch.tensor(rewards).float().unsqueeze(1).to(self.device)        
-        next_states = torch.from_numpy(np.array(next_states)).float().to(self.device)
-        dones = torch.tensor(dones).float().unsqueeze(1).to(self.device)
+    def choose_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
 
-        # Compute Q(s_t, a)
-        q_dist = self.q_network(states)
-        q_dist = q_dist.gather(2, actions.unsqueeze(2).expand(-1, -1, self.atom_size))
-
-        # Compute distributional Bellman update
+        state = torch.FloatTensor(state)
         with torch.no_grad():
-            next_action = (self.q_network(next_states) * self.support).sum(2).max(1)[1]
-            next_dist = self.target_network(next_states)[torch.arange(self.batch_size), next_action]
-            t_z = rewards + (1 - dones) * self.gamma * self.support.unsqueeze(0)
-            t_z = t_z.clamp(min=self.v_min, max=self.v_max)
-            b = (t_z - self.v_min) / self.delta_z
-            l = b.floor().long()
-            u = b.ceil().long()
-            d_m_l = (u.float() + (l == u).float() - b) * next_dist
-            d_m_u = (b - l.float()) * next_dist
-        m = states.new_zeros(self.batch_size, self.atom_size)
-        m.scatter_add_(1, l, d_m_l)
-        m.scatter_add_(1, u, d_m_u)
+            act_values = self.model(state)
+        return np.argmax(act_values.cpu().numpy())
 
-        # Update Q_Network
-        self.q_network.train()
-        self.optimizer.zero_grad()
-        loss = -(m * q_dist.log()).sum(1).mean()
-        loss.backward()
-        self.optimizer.step()
+    def replay(self):
+        if len(self.memory) < self.batch_size:
+            return
 
-        # Update target network
-        self.update_target_network()
+        minibatch = random.sample(self.memory, self.batch_size)
 
-    def update_target_network(self, tau=0.05):
-        for target_param, param in zip(self.target_network.parameters(), self.q_network.parameters()):
-            target_param.data.copy_(tau*param.data + (1.0-tau)*target_param.data)
+        for state, action, reward, next_state, done in minibatch:
+            target = self.model(torch.FloatTensor(state))
+            if done:
+                target[action] = reward
+            else:
+                target_val = self.target_model(torch.FloatTensor(next_state))
+                target[action] = reward + self.gamma * torch.max(target_val).item()
+
+            self.optimizer.zero_grad()
+            output = self.model(torch.FloatTensor(state))
+            loss = self.criterion(output, target.detach())
+            loss.backward()
+            self.optimizer.step()
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
 
     def learn(self, current_episode, total_episodes):
         state = self.env.reset()
-        state = np.reshape(state, [1, self.state_size])
+        state = np.reshape(state, [1, self.obs_size])
         for time in range(total_episodes):
-            action = self.get_action(state, current_episode)
+            action = self.choose_action(state[0])
             next_state, reward, done, _ = self.env.step(action)
-            next_state = np.reshape(next_state, [1, self.state_size])
-            self.remember(state, action, reward, next_state, done)
+            next_state = np.reshape(next_state, [1, self.obs_size])
+            self.remember(state[0], action, reward, next_state[0], done)
             state = next_state
-            if done:
-                # print("Episode: {}/{}, Score: {}" 
-                #         .format(current_episode, total_episodes, self.env.episode_reward))
+            if done:                
                 break
             if len(self.memory) > self.batch_size:
                 self.replay()
             
         return self.env.episode_reward
-    
+
     def save_model(self):
-        # Get current date and time
         now = datetime.datetime.now()
-
-        # Format datetime object to string
         now_str = now.strftime('%Y%m%d_%H%M%S')
-
-        # Save model weights with datetime in filename
-        torch.save(self.q_network.state_dict(), '../model/model_weights_' + now_str + '.pth')
+        torch.save(self.q_network.state_dict(), f'../model/model_weights_{now_str}.pth')
 
     def load_model(self, model_path):
         self.q_network.load_state_dict(torch.load(model_path))
         self.q_network.eval()
-
-    def perform_action(self, state):
-        state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
-        with torch.no_grad():
-            act_values = (self.q_network(state) * self.support).sum(dim=2)
-        return np.argmax(act_values.cpu().data.numpy())
--- train/qnetwork.py
+++ train/qnetwork.py
@@ -1,22 +1,15 @@
-import torch
 from torch import nn
 
 class QNetwork(nn.Module):
-    def __init__(self, state_size, action_size, atom_size, support, v_min, v_max):
+    def __init__(self, obs_size, action_size):
         super(QNetwork, self).__init__()
-        self.fc1 = nn.Linear(state_size, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, action_size * atom_size)
+        self.fc = nn.Sequential(
+            nn.Linear(obs_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, action_size)
+        )
 
-        self.v_min = v_min
-        self.v_max = v_max
-        self.atom_size = atom_size
-        self.delta_z = (self.v_max - self.v_min) / (self.atom_size - 1)
-        self.action_size = action_size
-        self.support = support
-
-    def forward(self, state):
-        x = torch.relu(self.fc1(state))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return torch.softmax(x.view(-1, self.action_size, self.atom_size), dim=2) * self.support
+    def forward(self, x):
+        return self.fc(x)
