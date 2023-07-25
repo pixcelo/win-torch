833c5919b9918aa0b814ff75556ecb45b1b892ba
pixcelo <bodomu@gmail.com>
2023-07-25 13:14:46 +0900

    mod: 状態にシャープレシオを追加

--- train/trading_env.py
+++ train/trading_env.py
@@ -2,6 +2,7 @@ import gym
 import numpy as np
 import pandas as pd
 from gym import spaces
+from reward_calculator import RewardCalculator
 
 class TradingEnv(gym.Env):
     def __init__(self, df, initial_cash=100000, transaction_cost=0.001, max_holdings=1):
@@ -10,12 +11,15 @@ class TradingEnv(gym.Env):
         self.df = df
         self.reward_range = (-np.inf, np.inf)
         self.action_space = spaces.Discrete(3)  # 0: Do nothing, 1: Buy, 2: Sell
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(self.df.shape[1] + 4,))
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(self.df.shape[1] + 5,))
 
+        self.reward_calculator = RewardCalculator(max_length=30)
         self.initial_cash = initial_cash
         self.transaction_cost = transaction_cost
         self.holdings = 0
         self.max_holdings = max_holdings
+        self.cumulative_rewards = 0
+        self.cumulative_rewards_squared = 0
         self.episode_reward = 0
         self.episode_rewards = []
         self.actions_history = []
@@ -83,4 +87,15 @@ class TradingEnv(gym.Env):
         total_asset = self.cash + self.holdings * self.df.iloc[self.current_step]['close']
         cash_ratio = self.cash / total_asset if total_asset > 0 else 0
         holdings_ratio = self.holdings * self.df.iloc[self.current_step]['close'] / total_asset if total_asset > 0 else 0
-        return np.append(self.df.iloc[self.current_step], [self.holdings, total_asset, cash_ratio, holdings_ratio])
+        
+        N = len(self.history) + 1  # the current number of steps
+        if N == 1:
+            sharpe_ratio = 0.0
+        else:
+            mean_rewards = self.cumulative_rewards / N
+            mean_rewards_squared = self.cumulative_rewards_squared / N
+            variance_rewards = mean_rewards_squared - mean_rewards**2
+            std_rewards = np.sqrt(variance_rewards)
+            sharpe_ratio = mean_rewards / std_rewards if std_rewards > 0 else 0.0
+
+        return np.append(self.df.iloc[self.current_step], [self.holdings, total_asset, cash_ratio, holdings_ratio, sharpe_ratio])
